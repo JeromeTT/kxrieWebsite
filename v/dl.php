3770bd49d3e752afcbfe9ba819e319332a5dd771
Kxrie <jaydenzhang5139@icloud.com>
2018-10-05 14:04:34 +1000

    initial php test

--- v/dl.php
+++ v/dl.php
@@ -1,3 +1,23 @@
 <?php
-echo ()
+$link = $_GET["inputLink"]
 ?>
+<!doctype HTMl>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="/main.css">
+        <title>YT Stat</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-primary">
+            <a class="navbar-brand" href="/">
+                <h1>Kxrie.me</h1>
+            </a>
+        </nav>
+        <h2 class="container p-3">ur link is <?php echo $link; ?></h2>
+    </body>
+    <footer class="footer"><div class="container" style="text-align: center;">
+         &copy; Jayden Zhang 2018
+    </div></footer>
+</html>
